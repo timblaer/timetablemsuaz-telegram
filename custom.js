3097fe276ba00fac2b203cd211714e490cd6ddc9
Teymur Bayramov <thetimblaer@gmail.com>
2018-10-07 17:50:08 +0400

    Info, fixes

--- custom.js
+++ custom.js
@@ -1,19 +1,19 @@
 const Database = require('./database.js');
 
-const moment = require('moment');
+const moment = require('moment-timezone');
 const resStrs = require('./result-strings.json');
 
 moment.locale('ru');
 
-const formatStr = 'DD MMMM YYYY в hh:mm';
-const fdate = (date) => moment(date).format(formatStr);
+const formatStr = 'DD MMMM в HH:mm';
+const fdate = (date) => moment.utc(date).tz('Asia/Baku').format(formatStr);
 
 const prettyPrint = (events) => events
                                 .map(e => `${resStrs.eventTypes[e.type]} *${e.title}*\n📅 ${fdate(e.date)}\n📍 ${e.place}\n`)
                                 .join('\n') || '[Пусто]';
 
-const getComplex = (types) => Database.models.Events.find({types: {$in: types}}).limit(10).sort({date: 1});
-const getSimple = () => Database.models.Events.find({type}).limit(10).sort({date: 1})
+const getComplex = (types) => Database.models.Events.find({type: {$in: types}}).limit(10).sort({date: 1});
+const getSimple = (type) => Database.models.Events.find({type}).limit(10).sort({date: 1})
 module.exports = {
     prettyPrint, getComplex, getSimple
 }
--- subscriptions.js
+++ subscriptions.js
@@ -4,9 +4,22 @@ const $_ = require("./consts.js");
 const subsMap = require('./subscriptions.json');
 const resStrs = require('./result-strings.json');
 
+const isArray = require('lodash/isArray');
+const isEmpty = require('lodash/isEmpty');
+
 async function handle (bot, chatId, msg, defaultKeyboardSettings) {
     const user = await Database.models.Users.findOne({chatId});
-    const action = user.subscriptions.indexOf(msg) > -1; //true for unsub, false for sub
+    const types = $_[subsMap[msg]];
+
+    let action;
+
+    if(isArray(types)) {
+        action = !isEmpty(types.filter(t => user.subscriptions.indexOf(t) > -1));
+    } else {
+        action = user.subscriptions.indexOf(types) > -1;
+    }
+    //true for unsub, false for sub
+
     const query = {};
 
     if(subsMap[msg] === undefined) {
@@ -14,17 +27,16 @@ async function handle (bot, chatId, msg, defaultKeyboardSettings) {
     }
 
     if(action) {
-        query["$pullAll"] = {subscriptions: $_[subsMap[msg]]};
+        query["$pullAll"] = {subscriptions: isArray(types) ? $_[subsMap[msg]] : [$_[subsMap[msg]]]};
     } else {
         query["$push"] = {subscriptions: $_[subsMap[msg]]};
     }
 
-    console.log(query);
-
     try {
-        await Database.models.Users.update({chatId}, query);
+        await Database.models.Users.updateOne({chatId}, query);
         bot.sendMessage(chatId, action ? resStrs.unsub : resStrs.sub, defaultKeyboardSettings);
     } catch (e) {
+        console.log(e);
         bot.sendMessage(chatId, resStrs.error, defaultKeyboardSettings);
     }
 
